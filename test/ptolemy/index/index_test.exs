1a8093e135b07cb22356d38c628c4ad8da400e9e
Louis Kureuil Person <louis@person.guru>
2019-04-21 22:29:41 +0200

    Fix Index tests

--- test/ptolemy/index/index_test.exs
+++ test/ptolemy/index/index_test.exs
@@ -10,18 +10,27 @@ defmodule Ptolemy.IndexTest do
     @update_attrs %{location: "some updated location", title: "some updated title"}
     @invalid_attrs %{location: nil, title: nil}
 
+    def owner_fixture() do
+      alias Ptolemy.Accounts
+
+      email = "louis@person.guru"
+      try do
+        Accounts.get_user_by_email!(email)
+      rescue
+        _ in Ecto.NoResultsError ->
+          {:ok, user} = Accounts.create_user(%{email: email})
+          user
+      end
+    end
+
     def link_fixture(attrs \\ %{}) do
+      owner = owner_fixture()
       {:ok, link} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Index.create_link()
+        |> Index.create_submit(owner)
 
       link
-    end
-
-    test "list_links/0 returns all links" do
-      link = link_fixture()
-      assert Index.list_links() == [link]
     end
 
     test "get_link!/1 returns the link with given id" do
@@ -30,25 +39,27 @@ defmodule Ptolemy.IndexTest do
     end
 
     test "create_link/1 with valid data creates a link" do
-      assert {:ok, %Link{} = link} = Index.create_link(@valid_attrs)
+      owner = owner_fixture()
+      assert {:ok, %Link{} = link} = Index.create_submit(@valid_attrs, owner)
       assert link.location == "some location"
       assert link.title == "some title"
     end
 
     test "create_link/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Index.create_link(@invalid_attrs)
+      owner = owner_fixture()
+      assert {:error, %Ecto.Changeset{}} = Index.create_submit(@invalid_attrs, owner)
     end
 
     test "update_link/2 with valid data updates the link" do
       link = link_fixture()
-      assert {:ok, %Link{} = link} = Index.update_link(link, @update_attrs)
+      assert {:ok, %Link{} = link} = Index.update_submit(link, @update_attrs)
       assert link.location == "some updated location"
       assert link.title == "some updated title"
     end
 
     test "update_link/2 with invalid data returns error changeset" do
       link = link_fixture()
-      assert {:error, %Ecto.Changeset{}} = Index.update_link(link, @invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Index.update_submit(link, @invalid_attrs)
       assert link == Index.get_link!(link.id)
     end
 
@@ -58,9 +69,8 @@ defmodule Ptolemy.IndexTest do
       assert_raise Ecto.NoResultsError, fn -> Index.get_link!(link.id) end
     end
 
-    test "change_link/1 returns a link changeset" do
-      link = link_fixture()
-      assert %Ecto.Changeset{} = Index.change_link(link)
+    test "change_submit/1 returns a submit changeset" do
+      assert %Ecto.Changeset{} = Index.change_submit(%Index.Submit{})
     end
   end
 end
