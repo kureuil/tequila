bcc375d058f4e4a0418bed0009d1e61309b3f93b
Louis Kureuil Person <louis@person.guru>
2019-04-21 21:38:03 +0200

    Fix PageController tests

--- lib/ptolemy_web/controllers/page_controller.ex
+++ lib/ptolemy_web/controllers/page_controller.ex
@@ -1,9 +1,7 @@
 defmodule PtolemyWeb.PageController do
   use PtolemyWeb, :controller
-  alias Ptolemy.Channels
 
   def index(conn, _params) do
-    default_channel = Channels.get_default_for_user(conn.assigns[:current_user])
-    redirect(conn, to: Routes.channel_path(conn, :show, default_channel))
+    redirect(conn, to: Routes.channel_path(conn, :index))
   end
 end
