5167fe63f69ad99fd38ffd61240c9a7260280a30
Louis Kureuil Person <louis@person.guru>
2019-04-22 14:16:53 +0200

    Launch postgresql service on Travis CI

--- config/test.exs
+++ config/test.exs
@@ -11,9 +11,9 @@ config :logger, level: :warn
 
 # Configure your database
 config :ptolemy, Ptolemy.Repo,
-  username: "ptolemy",
-  password: "imnotmeantforproduction",
-  database: "ptolemy_test",
-  hostname: "localhost",
+  username: System.get_env("TEST_DATABASE_USER") || "ptolemy",
+  password: System.get_env("TEST_DATABASE_PASS") || "imnotmeantforproduction",
+  database: System.get_env("TEST_DATABASE_NAME") || "ptolemy_test",
+  hostname: System.get_env("TEST_DATABASE_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
   migration_primary_key: [name: :id, type: :binary_id]
