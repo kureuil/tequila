7bb9a23043dcb40ce6893cb65bcf0f7c7921d591
Louis "Kureuil" Person <louis@person.guru>
2019-09-06 22:22:35 +0200

    mix format

--- config/releases.exs
+++ config/releases.exs
@@ -3,7 +3,11 @@ import Config
 config :tequila, TequilaWeb.Endpoint,
   http: [port: System.get_env("PORT", "8378") |> Integer.parse() |> elem(0)],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  url: [host: {:system, "URL_HOST"}, port: {:system, "URL_PORT"}, scheme: System.get_env("URL_SCHEME", "https")]
+  url: [
+    host: {:system, "URL_HOST"},
+    port: {:system, "URL_PORT"},
+    scheme: System.get_env("URL_SCHEME", "https")
+  ]
 
 config :tequila, Tequila.Repo,
   url: System.get_env("DATABASE_URL"),
