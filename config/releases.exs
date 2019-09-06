6025cd0cb8aef5db45037fd926ba5bab8f5755e9
Louis "Kureuil" Person <louis@person.guru>
2019-09-06 22:22:18 +0200

    Fix URL scheme configuration
    The :scheme option for url generation only accepts binaries.

--- config/releases.exs
+++ config/releases.exs
@@ -1,15 +1,9 @@
 import Config
 
-scheme =
-  case System.get_env("URL_SCHEME", "https") do
-    "http" -> :http
-    _ -> :https
-  end
-
 config :tequila, TequilaWeb.Endpoint,
   http: [port: System.get_env("PORT", "8378") |> Integer.parse() |> elem(0)],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  url: [host: {:system, "URL_HOST"}, port: {:system, "URL_PORT"}, scheme: scheme]
+  url: [host: {:system, "URL_HOST"}, port: {:system, "URL_PORT"}, scheme: System.get_env("URL_SCHEME", "https")]
 
 config :tequila, Tequila.Repo,
   url: System.get_env("DATABASE_URL"),
