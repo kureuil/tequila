3df0a17054fea45f0529d771c83dfdcaa91f3ab0
Louis "Kureuil" Person <louis@person.guru>
2019-09-11 19:39:24 +0200

    mix format

--- lib/tequila_web/endpoint.ex
+++ lib/tequila_web/endpoint.ex
@@ -39,7 +39,8 @@ defmodule TequilaWeb.Endpoint do
   # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session,
     store: :cookie,
-    key: (if System.get_env("URL_SCHEME") == "https", do: "__Host-tequila_key", else: "_tequila_key"),
+    key:
+      if(System.get_env("URL_SCHEME") == "https", do: "__Host-tequila_key", else: "_tequila_key"),
     signing_salt: "JPY8ZFCC",
     secure: System.get_env("URL_SCHEME") == "https",
     extra: "SameSite=Lax"
--- lib/tequila_web/plugs/helmet_plug.ex
+++ lib/tequila_web/plugs/helmet_plug.ex
@@ -3,15 +3,26 @@ defmodule TequilaWeb.HelmetPlug do
 
   def init(_opts) do
     opts = []
-    opts = case Mix.env do
-      :prod -> Keyword.put(opts, :csp, "default-src 'self'")
-      _ -> Keyword.put(opts, :csp, "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'")
-    end
+
+    opts =
+      case Mix.env() do
+        :prod ->
+          Keyword.put(opts, :csp, "default-src 'self'")
+
+        _ ->
+          Keyword.put(
+            opts,
+            :csp,
+            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'"
+          )
+      end
+
     opts
   end
 
   def call(conn, opts) do
     csp = Keyword.get(opts, :csp, "default-src 'self'")
+
     conn
     |> put_resp_header("referrer-policy", "strict-origin")
     |> put_resp_header("content-security-policy", csp)
