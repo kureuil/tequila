2285a81256d1394bf2533b67e122d740c16fc20c
Louis "Kureuil" Person <louis@person.guru>
2019-09-11 23:58:36 +0200

    Configure session plug at runtime instead of compile-time

--- lib/tequila_web/plugs/session_plug.ex
+++ lib/tequila_web/plugs/session_plug.ex
@@ -1,5 +1,17 @@
 defmodule TequilaWeb.SessionPlug do
   def init(_opts) do
+    # The session will be stored in the cookie and signed,
+    # this means its contents can be read but not tampered with.
+    # Set :encryption_salt if you would also like to encrypt it.
+    Plug.Session.init(
+      store: :cookie,
+      key: "_tequila_key",
+      signing_salt: "JPY8ZFCC",
+      extra: "SameSite=Lax"
+    )
+  end
+
+  def call(conn, opts) do
     scheme = TequilaWeb.Endpoint.struct_url().scheme
 
     key =
@@ -11,19 +23,11 @@ defmodule TequilaWeb.SessionPlug do
           "_tequila_key"
       end
 
-    # The session will be stored in the cookie and signed,
-    # this means its contents can be read but not tampered with.
-    # Set :encryption_salt if you would also like to encrypt it.
-    Plug.Session.init(
-      store: :cookie,
-      key: key,
-      signing_salt: "JPY8ZFCC",
-      secure: scheme == "https",
-      extra: "SameSite=Lax"
-    )
-  end
+    opts =
+      opts
+      |> Map.put(:key, key)
+      |> Map.put(:cookie_opts, Keyword.put(opts[:cookie_opts], :secure, scheme == "https"))
 
-  def call(conn, opts) do
     Plug.Session.call(conn, opts)
   end
 end
