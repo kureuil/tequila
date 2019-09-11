0bfbbd9dcf5ee01cfce484f5fe997867fe0a137f
Louis Person <louis@person.guru>
2019-09-11 19:43:54 +0200

    Improve security by using built-in browsers mechanisms (#39)
    This commit makes cookies a lot more secure than before, especially if
    tequila is served over HTTPS. It also adds the Referrer-Policy and
    Content-Security-Policy headers to every response served through the
    :browser router pipeline.

--- lib/tequila_web/router.ex
+++ lib/tequila_web/router.ex
@@ -7,6 +7,7 @@ defmodule TequilaWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug TequilaWeb.HelmetPlug
   end
 
   pipeline :require_authenticated do
